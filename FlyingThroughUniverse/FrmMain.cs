92d8aef9e7e6ac920345bfc9e206970e12b231c4
TheAltruist <m.jorge.romero@gmail.com>
2015-10-15 03:24:37 -0300

    refactored

--- FlyingThroughUniverse/FrmMain.cs
+++ FlyingThroughUniverse/FrmMain.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
-//using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FlyingThroughUniverse
@@ -12,7 +10,6 @@ namespace FlyingThroughUniverse
     public partial class FrmMain : Form
     {
         protected Canvas CanvasSurface { get; set; }
-        protected List<Star> Stars { get; set; }
         
         public FrmMain()
         {
@@ -26,14 +23,11 @@ namespace FlyingThroughUniverse
             ForeColor = Color.White;
 
             CanvasSurface = new Canvas(Width, Height);
-            Stars = new List<Star>();
-            CreateStars();
-            Travel();
+            Travel(CreateObservableUniverse());
         }
 
-        private void Travel()
+        private void Travel(IObservable<Star> observableStars)
         {
-            var observableStars = Stars.ToObservable(Scheduler.Default).Repeat();
             observableStars.ObserveOn(Scheduler.CurrentThread).Subscribe(star =>
             {
                 Circle(Color.Black, star.X, star.Y, star.Distance);
@@ -42,20 +36,22 @@ namespace FlyingThroughUniverse
             });
         }
 
+        private IObservable<Star> CreateObservableUniverse()
+        {
+            const int totalStars = 100;
+            var randomSeed = new Random();
+            var stars = Enumerable.Range(0, totalStars)
+                .Select(x => new Star(CanvasSurface, randomSeed))
+                .ToList()
+                .ToObservable(Scheduler.Default)
+                .Repeat();
+            return stars;
+        }
+
         private void Circle(Color color, double x, double y, double diameter)
         {
             var myGraphics = CreateGraphics();
             myGraphics.FillEllipse(new SolidBrush(color), (float)x, (float)y, (float)diameter, (float)diameter);
-        }
-        private void CreateStars()
-        {
-            const int totalStars = 100;
-            var randomSeed = new Random();
-            
-            for (var i = 0; i < totalStars; i++)
-            {
-                Stars.Add(new Star(CanvasSurface, randomSeed));
-            }
         }
 
         private void FrmMain_MouseDoubleClick(object sender, MouseEventArgs e)
